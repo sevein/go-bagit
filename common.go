156c148cb2c1bbb946bf9eb7ae352d295a9ea60b
Jesús García Crespo <jesus@sevein.com>
2022-02-16 20:16:09 +0100

    Build software agent with debub.ReadBuildInfo

--- common.go
+++ common.go
@@ -3,12 +3,21 @@ package go_bagit
 import (
 	"fmt"
 	"time"
+	"runtime/debug"
 )
+
 
 var currentTime = time.Now()
 
 var libraryVersion = "v0.1.1-alpha"
 
 func GetSoftwareAgent() string {
-	return fmt.Sprintf("go-bagit %s <https://github.com/nyudlts/go-bagit>", libraryVersion)
+	const url = "github.com/nyudlts/go-bagit"
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return fmt.Sprintf("go-bagit <%s>", url)
+	}
+
+	return fmt.Sprintf("go-bagit %s <%s>", info.Main.Version, url)
 }
